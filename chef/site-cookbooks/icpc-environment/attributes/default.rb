d1dc8bb63fe2707f2527d14e4ab8bbe4eee52f34
Keith Johnson <kj@ubergeek42.com>
2013-10-10 15:54:24 -0400

    Remove submodules(they don't work)

--- chef/site-cookbooks/icpc-environment/attributes/default.rb
+++ chef/site-cookbooks/icpc-environment/attributes/default.rb
@@ -4,8 +4,8 @@
 default['contestenv']['username'] = 'contestant'
 
 # Git repositories to pull skeleton directories from
-default['contestenv']['contestant_home_repo'] = '/dist/contestant_home/.git'
-default['contestenv']['admin_home_repo'] = '/dist/admin_home/.git'
+default['contestenv']['contestant_home_repo'] = 'https://github.com/icpc-environment/contestant_home.git'
+default['contestenv']['admin_home_repo'] = 'https://github.com/icpc-environment/admin_home.git'
 
 default['contestenv']['jdk32'] = '/dist/jdk-7u40-linux-i586.tar.gz'
 default['contestenv']['jdk64'] = '/dist/jdk-7u40-linux-x64.tar.gz'
